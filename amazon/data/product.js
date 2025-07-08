7d3c1206a66e2287c296b52a4bf44fbbc8dd8507
Lubna Febin <lubnafebin13@gmail.com>
2025-07-08 20:19:48 +0530

    Implement dynamic product rendering on Amazon page

--- amazon/data/product.js
+++ amazon/data/product.js
@@ -91,7 +91,7 @@ const products = [
   },
   {
     id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-    image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Plain Hooded Fleece Sweatshirt",
     rating: {
       stars: 4.5,
@@ -106,7 +106,7 @@ const products = [
   },
   {
     id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
-    image: "images/products/luxury-tower-set-6-piece.jpg",
+    image: "images/products/black-2-slot-toaster.jpg",
     name: "Luxury Towel Set - Graphite Gray",
     rating: {
       stars: 4.5,
@@ -123,7 +123,7 @@ const products = [
   },
   {
     id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
-    image: "images/products/liquid-laundry-detergent-plain.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
     rating: {
       stars: 4.5,
@@ -137,7 +137,7 @@ const products = [
   },
   {
     id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
-    image: "images/products/knit-athletic-sneakers-gray.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Waterproof Knit Athletic Sneakers - Gray",
     rating: {
       stars: 4,
@@ -152,7 +152,7 @@ const products = [
   },
   {
     id: "5968897c-4d27-4872-89f6-5bcb052746d7",
-    image: "images/products/women-chiffon-beachwear-coverup-black.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Women's Chiffon Beachwear Cover Up - Black",
     rating: {
       stars: 4.5,
@@ -171,7 +171,7 @@ const products = [
   },
   {
     id: "aad29d11-ea98-41ee-9285-b916638cac4a",
-    image: "images/products/round-sunglasses-black.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Round Sunglasses",
     rating: {
       stars: 4.5,
@@ -185,7 +185,7 @@ const products = [
   },
   {
     id: "04701903-bc79-49c6-bc11-1af7e3651358",
-    image: "images/products/women-beach-sandals.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Women's Two Strap Buckle Sandals - Tan",
     rating: {
       stars: 4.5,
@@ -202,7 +202,7 @@ const products = [
   },
   {
     id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
-    image: "images/products/blackout-curtain-set-beige.webp",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Blackout Curtains Set 4-Pack - Beige",
     rating: {
       stars: 4.5,
@@ -217,7 +217,7 @@ const products = [
   },
   {
     id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
-    image: "images/products/men-slim-fit-summer-shorts-gray.jpg",
+    image: "images/products/intermediate-composite-basketball.jpg",
     name: "Men's Slim-Fit Summer Shorts",
     rating: {
       stars: 4,
@@ -232,7 +232,7 @@ const products = [
   },
   {
     id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
-    image: "images/products/electric-glass-and-steel-hot-water-kettle.webp",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
     rating: {
       stars: 5,
@@ -247,7 +247,7 @@ const products = [
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
-    image: "images/products/facial-tissue-2-ply-18-boxes.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Ultra Soft Tissue 2-Ply - 18 Box",
     rating: {
       stars: 4,
@@ -264,7 +264,7 @@ const products = [
   },
   {
     id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
-    image: "images/products/straw-sunhat.webp",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Straw Lifeguard Sun Hat",
     rating: {
       stars: 4,
@@ -280,7 +280,7 @@ const products = [
   },
   {
     id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
-    image: "images/products/sky-flower-stud-earrings.webp",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Sterling Silver Sky Flower Stud Earrings",
     rating: {
       stars: 4.5,
@@ -295,7 +295,7 @@ const products = [
   },
   {
     id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
-    image: "images/products/women-stretch-popover-hoodie-black.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Women's Stretch Popover Hoodie",
     rating: {
       stars: 4.5,
@@ -314,7 +314,7 @@ const products = [
   },
   {
     id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
-    image: "images/products/bathroom-rug.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
     rating: {
       stars: 4.5,
@@ -329,7 +329,7 @@ const products = [
   },
   {
     id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
-    image: "images/products/women-knit-ballet-flat-black.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Women's Knit Ballet Flat",
     rating: {
       stars: 4,
@@ -345,7 +345,7 @@ const products = [
   },
   {
     id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
-    image: "images/products/men-golf-polo-t-shirt-blue.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
     rating: {
       stars: 4.5,
@@ -363,7 +363,7 @@ const products = [
   },
   {
     id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
-    image: "images/products/trash-can-with-foot-pedal-50-liter.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Trash Can with Foot Pedal - Brushed Stainless Steel",
     rating: {
       stars: 4.5,
@@ -379,7 +379,7 @@ const products = [
   },
   {
     id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
-    image: "images/products/duvet-cover-set-blue-twin.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Duvet Cover Set with Zipper Closure",
     rating: {
       stars: 4,
@@ -396,7 +396,7 @@ const products = [
   },
   {
     id: "d2785924-743d-49b3-8f03-ec258e640503",
-    image: "images/products/women-chunky-beanie-gray.webp",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Women's Chunky Cable Beanie - Gray",
     rating: {
       stars: 5,
@@ -414,7 +414,7 @@ const products = [
   },
   {
     id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
-    image: "images/products/men-chino-pants-beige.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Men's Classic-fit Pleated Chino Pants",
     rating: {
       stars: 4.5,
@@ -429,7 +429,7 @@ const products = [
   },
   {
     id: "1c079479-8586-494f-ab53-219325432536",
-    image: "images/products/men-athletic-shoes-green.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Men's Athletic Sneaker",
     rating: {
       stars: 4,
@@ -445,7 +445,7 @@ const products = [
   },
   {
     id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
-    image: "images/products/men-navigator-sunglasses-brown.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Men's Navigator Sunglasses Pilot",
     rating: {
       stars: 3.5,
@@ -461,7 +461,7 @@ const products = [
   },
   {
     id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
-    image: "images/products/non-stick-cooking-set-15-pieces.webp",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
     rating: {
       stars: 4.5,
@@ -475,7 +475,7 @@ const products = [
   },
   {
     id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
-    image: "images/products/vanity-mirror-silver.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Vanity Mirror with Heavy Base - Chrome",
     rating: {
       stars: 4.5,
@@ -491,7 +491,7 @@ const products = [
   },
   {
     id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
-    image: "images/products/women-french-terry-fleece-jogger-camo.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Women's Fleece Jogger Sweatpant",
     rating: {
       stars: 4.5,
@@ -508,7 +508,7 @@ const products = [
   },
   {
     id: "d339adf3-e004-4c20-a120-40e8874c66cb",
-    image: "images/products/double-elongated-twist-french-wire-earrings.webp",
+    image: "images/products/intermediate-composite-basketball.jpg",
     name: "Double Oval Twist French Wire Earrings - Gold",
     rating: {
       stars: 4.5,
@@ -522,7 +522,7 @@ const products = [
   },
   {
     id: "d37a651a-d501-483b-aae6-a9659b0757a0",
-    image: "images/products/round-airtight-food-storage-containers.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Round Airtight Food Storage Containers - 5 Piece",
     rating: {
       stars: 4,
@@ -537,7 +537,7 @@ const products = [
   },
   {
     id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
-    image: "images/products/coffeemaker-with-glass-carafe-black.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
     rating: {
       stars: 4.5,
@@ -552,7 +552,7 @@ const products = [
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
-    image: "images/products/blackout-curtains-black.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
     rating: {
       stars: 4.5,
@@ -566,7 +566,7 @@ const products = [
   },
   {
     id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
-    image: "images/products/cotton-bath-towels-teal.webp",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "100% Cotton Bath Towels - 2 Pack, Light Teal",
     rating: {
       stars: 4.5,
@@ -581,7 +581,7 @@ const products = [
   },
   {
     id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
-    image: "images/products/knit-athletic-sneakers-pink.webp",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Waterproof Knit Athletic Sneakers - Pink",
     rating: {
       stars: 4,
@@ -597,7 +597,7 @@ const products = [
   },
   {
     id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
-    image: "images/products/countertop-blender-64-oz.jpg",
+    image: "images/products/intermediate-composite-basketball.jpg",
     name: "Countertop Blender - 64oz, 1400 Watts",
     rating: {
       stars: 4,
@@ -612,7 +612,7 @@ const products = [
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
-    image: "images/products/floral-mixing-bowl-set.jpg",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
     name: "10-Piece Mixing Bowl Set with Lids - Floral",
     rating: {
       stars: 5,
@@ -628,7 +628,7 @@ const products = [
   },
   {
     id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
-    image: "images/products/kitchen-paper-towels-30-pack.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "2-Ply Kitchen Paper Towels - 30 Pack",
     rating: {
       stars: 4.5,
@@ -643,7 +643,7 @@ const products = [
   },
   {
     id: "bc2847e9-5323-403f-b7cf-57fde044a955",
-    image: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
+    image: "images/products/intermediate-composite-basketball.jpg",
     name: "Men's Full-Zip Hooded Fleece Sweatshirt",
     rating: {
       stars: 4.5,
