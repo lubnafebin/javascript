45a2900d8913ae31e2996a0be9eac36fdfcc9dd4
Lubna Febin <lubnafebin13@gmail.com>
2025-11-21 19:26:34 +0530

    refactor: reset initial game state and update newCard function to draw random card

--- black-jack/index.js
+++ black-jack/index.js
@@ -1,16 +1,34 @@
-let firstCard = 10;
-let secondCard = 4;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hashBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById("message");
 let sumEl = document.getElementById("sum");
 let cardEl = document.getElementById("card");
 
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
   cardEl.textContent = "card :";
 
   for (let i = 0; i < cards.length; i++) {
@@ -31,8 +49,8 @@ function startGame() {
 }
 
 function newCard() {
-  let card = 6;
+  let card = getRandomCard();
   sum += card;
   cards.push(card);
-  startGame();
+  renderGame();
 }
