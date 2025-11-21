39d2b03f9c61f1ed08eccd5c420b56ae1e31fec2
Lubna Febin <lubnafebin13@gmail.com>
2025-11-21 18:54:49 +0530

    refactor: organize card handling in startGame function

--- black-jack/index.js
+++ black-jack/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hashBlackJack = false;
 let isAlive = true;
@@ -8,8 +9,14 @@ let message = "";
 let messageEl = document.getElementById("message");
 let sumEl = document.getElementById("sum");
 let cardEl = document.getElementById("card");
+
 function startGame() {
-  cardEl.textContent = "card :" + firstCard + " " + secondCard;
+  cardEl.textContent = "card :";
+
+  for (let i = 0; i < cards.length; i++) {
+    cardEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum :" + sum;
   if (sum <= 20) {
     message = "do you want to draw a new card?";
@@ -21,4 +28,8 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+
+function newCard() {
+  
 }
